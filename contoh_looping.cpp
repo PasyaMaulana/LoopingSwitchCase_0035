e557b34dee59c2663a729082b9108f3fa2d1e127
PasyaMaulana <pasyamaulana17@gmail.com>
2025-03-17 10:57:31 +0700

    perulangan pencacah naik

--- contoh_looping.cpp
+++ contoh_looping.cpp
@@ -8,4 +8,14 @@ int main()
     int n;
     int perulanganWhile;
     int perulanganDo;
+
+    // perulangan dengan for
+
+    cout << "Perulangan for pencacah naik" << endl;
+    for (n = 0; n < 5; n++)
+    {
+        cout << "Nilai n = " << n << "Selamat Datang" << endl;
+    }
+    cout << "Nilai n terakhir = " << n << endl;
+    cout << endl:
 }
